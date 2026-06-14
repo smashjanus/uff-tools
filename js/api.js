e57af93ef400e042e410cb4e6058cf215ac2af3e
smashjanus <smashjanus@gmail.com>
2026-06-14 13:26:05 -0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxeFxtcIBX6Ylqe61rBabiKbLJ7q6UGI6u2yf3-wFnFF9Qw9dg8zhdAJynTDUS-DYy4Aw/exec';
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyCAtPb9jFRHBtgr1LyJbX8CLeA_TkIzyntgSfoe7EfKiyMPBkbhFMRmf0XvVI-KF1G/exec';
 
 async function gasRequest(action, payload = {}) {
   if (!GAS_WEB_APP_URL) {
